a9bc163950cf64cff54a6a35e5555897d98643e0
yamamoto <tenfive1227@gmail.com>
2021-01-08 12:18:29 +0900

    自PC内で通信してCubeの複製・cubeごとの移動の作成

---
+++ NetworkTest/Assets/CubeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class CubeController : NetworkBehaviour
+{
+    [SerializeField] float speed = 0.03f;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (!isLocalPlayer) return;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(speed * -1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, speed * -1);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed, 0, 0);
+        }
+    }
+}
